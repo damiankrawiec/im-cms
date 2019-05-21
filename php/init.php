f39c884383491d47ff85872c1ee67a0ff9ca7bb9
damiankrawiec <38858155+damiankrawiec@users.noreply.github.com>
2019-05-21 10:47:48 +0200

    Inicjalizacja systemu domenowego

--- php/init.php
+++ php/init.php
@@ -1,2 +1,11 @@
 <?php
 require_once 'app/composer/vendor/autoload.php'; // Autoload files using Composer autoload
+
+require_once 'php/class/App.php';
+
+$app = new App();
+
+$appData = array(
+    'domain' => $app->getUrl('HTTP_HOST')
+);
+
