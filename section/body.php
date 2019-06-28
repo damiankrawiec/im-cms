bf61b938f175e0e5b95fd7393bfa5b0f43834ab9
damiank-aqua <50693720+damiank-aqua@users.noreply.github.com>
2019-06-28 12:03:49 +0200

    Removed slider. Current slider is from object with only slider (images) field. Section head and body clear (pgwslider)

--- section/body.php
+++ section/body.php
@@ -1,9 +1,3 @@
 <script src="app/composer/vendor/components/jquery/jquery.min.js"></script>
 <script src="app/composer/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script src="app/composer/vendor/components/jqueryui/jquery-ui.min.js"></script>
-<?php
-if($url == $s_startSection) {
-
-    echo '<script src="module/pgwslider/pgwslider.min.js"></script>';
-
-}
--- section/head.php
+++ section/head.php
@@ -2,9 +2,3 @@
 <title><?php echo $system->getSection()->name; ?></title>
 <link rel="stylesheet" href="app/composer/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="module/fontawesome/css/all.min.css">
-<?php
-if($url == $s_startSection) {
-
-    echo '<link rel="stylesheet" href="module/pgwslider/pgwslider.min.css">';
-
-}
