e9f4d8b5d58652a75779c1665419688381e7d4f9
damiankrawiec <38858155+damiankrawiec@users.noreply.github.com>
2019-07-18 21:28:43 +0200

    Admin auth - starting

---
+++ admin/auth/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../layout/css/main.css">
+</head>
+<body>
+<div id="background">
+<div class="half-window"></div>
+</div>
+</body>
+</html>
