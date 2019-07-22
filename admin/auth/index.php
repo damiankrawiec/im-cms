3ee2840afa724543e93d7b335ac6dc9ee55726eb
damiank-aqua <50693720+damiank-aqua@users.noreply.github.com>
2019-07-22 12:06:05 +0200

    Login admin - RWD

--- admin/auth/index.php
+++ admin/auth/index.php
@@ -11,32 +11,36 @@
 </head>
 <body>
 <div id="background">
-<div class="half-window">
     <div class="container">
-        <div class="row">
-            <div class="col-4">
+        <div class="row window-back">
+            <div class="col-12 col-lg-8">
+                <div class="login-left">
                 <div id="auth-header">IM-CMS/auth</div>
-                <form>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-dark">Submit</button>
-                </form>
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-dark">Submit</button>
+                    </form>
+                </div>
             </div>
-            <div class="col-8"></div>
+            <div class="d-none d-lg-block d-xl-block col-lg-4" id="login-right">
+                <div class="login-right">
+                    <img src="../layout/graphic/auth.png">
+                </div>
+            </div>
         </div>
     </div>
-</div>
 </div>
 <script src="../../app/composer/vendor/components/jquery/jquery.min.js"></script>
 <script src="../../app/composer/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
