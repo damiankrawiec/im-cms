cb8440bc7e3256eeb8fc94f73b41007fef9d43cd
damiank-aqua <50693720+damiank-aqua@users.noreply.github.com>
2019-07-30 11:27:49 +0200

    Update fontawesome to var. 5.10 (duotone)

--- cms/config/icon.php
+++ cms/config/icon.php
@@ -1,12 +1,12 @@
 <?php
 
 $icon = array(
-    'email' => array('at' => '<i class="far fa-at"></i>'),
-    'security' => array('header' => '<i class="fal fa-shield-check fa-3x"></i>', 'password' => '<i class="far fa-lock-alt"></i>'),
+    'email' => array('at' => '<i class="fal fa-at"></i>'),
+    'security' => array('header' => '<i class="fad fa-shield-check fa-3x"></i>', 'password' => '<i class="fal fa-lock-alt"></i>'),
     'login' => array('standard' => '<i class="fal fa-sign-in"></i>'),
     'external-link' => array('square' => '<i class="fal fa-external-link-square"></i>'),
     'empty-triangle' => array('right' => '<i class="fal fa-caret-right"></i>'),
-    'warning' => array('validation' => '<i class="fal fa-exclamation-triangle text-danger" style="font-size:26px;margin:5px"></i>'),
+    'warning' => array('validation' => '<i class="fad fa-exclamation-triangle text-danger" style="font-size:26px;margin:5px"></i>'),
     'process' => array('standard' => '<i class="fas fa-asterisk fa-spin" style="font-size:13px;margin-left:5px"></i>'),
-    'message' => array('information' => '<i class="fal fa-comment-alt-lines"></i>', 'alert' => '<i class="fal fa-comment-alt"></i>')
+    'message' => array('information' => '<i class="fal fa-comment-alt-lines"></i>', 'alert' => '<i class="fad fa-exclamation"></i>')
 );
