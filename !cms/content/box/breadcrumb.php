ab80473e59cff47b310a2d7d694a4f77997c7e23
damiankrawiec <38858155+damiankrawiec@users.noreply.github.com>
2020-11-08 12:03:35 +0100

    [CMS] Object description in main menu [CMS] Breadcrumb of url's history

--- !cms/content/box/breadcrumb.php
+++ !cms/content/box/breadcrumb.php
@@ -4,7 +4,7 @@ if(is_array(($tool->getSession('breadcrumb'))) and count(($tool->getSession('bre
 
     $breadcrumbArray = array_reverse($tool->getSession('breadcrumb'));
 
-    echo '<div class="clearfix p-1">';
+    echo '<div class="clearfix p-1 text-center">';
     foreach($breadcrumbArray as $ba) {
 
         echo '<a href="'.$ba.'" title="'.$ba.'" class="btn btn-secondary m-1">'.$ba.'</a>';
