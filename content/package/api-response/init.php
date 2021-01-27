f2f94446e4bdf7081fd40e8c2f526c3742e1c5e2
damiankrawiec <38858155+damiankrawiec@users.noreply.github.com>
2021-01-27 19:40:11 +0100

    Package update

--- content/package/api-response/init.php
+++ content/package/api-response/init.php
@@ -3,3 +3,6 @@
 //$path source of package
 
 echo 'api response code run';
+
+//when is form (parameter)
+//require 'content/package/submit.php';
---
+++ content/package/submit.php
@@ -0,0 +1,5 @@
+<?php
+echo '<input type="submit" value="' . $this->makeTranslationSystem('run') . '" class="btn btn-danger run-package"> <span class="im-hide">'.$this->icon['process']['spin'].'</span>';
+echo '<input type="hidden" name="package" value="' . $nextPackage . '">';
+echo '<input type="hidden" name="id" value="' . $dataId . '">';
+echo '<input type="hidden" name="transaction_package" value="'.$this->addition->transaction().'">';
