d58692191a6a9c2a8e45083305f13e695f638977
damiank-aqua <50693720+damiank-aqua@users.noreply.github.com>
2021-01-28 14:40:08 +0100

    Package - data table Definition: {  "init": {},  "data": {   "name": "data-table"  } }

--- content/package/data-table/init.php
+++ content/package/data-table/init.php
@@ -4,6 +4,31 @@ $apiResponsePath = 'content/package/api-response';
 
 $apiResponse = require_once $apiResponsePath.'/init.php';
 
-echo 'this data in datatable:';
-
 var_dump($apiResponse);
+
+$dataType = array('nowe', 'suma', 'aktywne');
+
+echo '<table class="table table-hover data-table">';
+
+    foreach ($apiResponse as $api) {
+
+        echo '<tr>';
+
+            echo '<td>'.$api->teryt_terc.' (dzień: '.$api->dzien.')</td>';
+
+        echo '</tr>';
+
+//            foreach ($api->{current($dataType)} as $i => $n) {
+//
+//                echo '<tr>';
+//
+//                echo '<td>'.$i.': '.$n.'</td>';
+//
+//                echo '</tr>';
+//
+//            }
+
+
+    }
+
+echo '</table>';
